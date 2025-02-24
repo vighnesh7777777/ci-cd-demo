61912bed12a7a28f6a68a03a9ab66c3095bb33d1
vighnesh7777777 <karna20033002@gmail.com>
2025-02-24 08:49:52 +0530

    value is added

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ def add(a, b):
     return a + b + c
 
 def test_add():
-    assert add(2, 3) == 5  # Test case
+    assert add(2, 3 ,5) == 10  # Test case
 
 if __name__ == "__main__":
     print("Addition:", add(2, 3))
